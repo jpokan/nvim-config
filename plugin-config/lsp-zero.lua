085b65886a669462d610ac4eec6c5cdb083a1d20
jpokan <jorgeou1993@gmail.com>
2023-12-22 20:08:23 -0500

    fix: enable format on save

--- plugin-config/lsp-zero.lua
+++ plugin-config/lsp-zero.lua
@@ -6,6 +6,17 @@ lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
+lsp_zero.format_on_save({
+  format_opts = {
+    async = false,
+    timeout_ms = 10000,
+  },
+  servers = {
+    ['tsserver'] = {'javascript', 'typescript'},
+	['volar'] = {'vue'}
+  }
+})
+
 -- see :help lsp-zero-guide:integrate-with-mason-nvim
 -- to learn how to use mason.nvim with lsp-zero
 require('mason').setup({})
