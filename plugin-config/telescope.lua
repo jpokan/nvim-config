fadcf35f1a0eb37cb61d33baaeb123997fbccce4
jpokan <jorgeou1993@gmail.com>
2024-01-03 13:53:24 -0500

    fix: remap C-y to open vsplit

--- plugin-config/telescope.lua
+++ plugin-config/telescope.lua
@@ -1,3 +1,11 @@
-require('telescope').setup({
+local actions = require("telescope.actions")
 
-})
+require('telescope').setup {
+	defaults = {
+		mappings = {
+			i = {
+				["<C-y>"] = actions.select_vertical,
+			}
+		}
+	}
+}
