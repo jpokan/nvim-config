45813164e59439735a551faeceb0b80ad97bb7fc
jpokan <jorgeou1993@gmail.com>
2024-04-22 12:13:21 -0500

    fix: use formatter

--- plugin-config/formatter.lua
+++ plugin-config/formatter.lua
@@ -1,21 +1,21 @@
--- require("formatter").setup {
--- 	filetype = {
--- 		vue = {
--- 			function()
--- 				return {
--- 					exe = "prettierd",
--- 					args = { vim.api.nvim_buf_get_name(0) },
--- 					stdin = true
--- 				}
--- 			end
--- 		}
--- 	},
--- }
---
--- local augroup = vim.api.nvim_create_augroup
--- local autocmd = vim.api.nvim_create_autocmd
--- augroup("__formatter__", { clear = true })
--- autocmd("BufWritePost", {
--- 	group = "__formatter__",
--- 	command = ":FormatWrite",
--- })
+require("formatter").setup {
+	filetype = {
+		vue = {
+			function()
+				return {
+					exe = "prettierd",
+					args = { vim.api.nvim_buf_get_name(0) },
+					stdin = true
+				}
+			end
+		}
+	},
+}
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+augroup("__formatter__", { clear = true })
+autocmd("BufWritePost", {
+	group = "__formatter__",
+	command = ":FormatWrite",
+})
